3ef886e39f9327c750e9412f8684bc2d93efeba9
Anthony Erlinger <aerlinger@gmail.com>
2012-12-01 09:23:13 -0800

    remove component/*.js files

--- src/component/components/JFetElm.js
+++
@@ -1,8 +0,0 @@
-function JFetElm(xa, ya, xb, yb, f, st) {
-    CircuitElement.call(this, xa, ya, xb, yb, f);
-
-}
-;
-
-JFetElm.prototype = new CircuitElement();
-JFetElm.prototype.constructor = JFetElm;
