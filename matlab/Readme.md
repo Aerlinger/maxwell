3f78d9fc3a9a31d6a7cefed20d361dfb85632f9c
Anthony Erlinger <aerlinger@gmail.com>
2014-12-28 02:55:38 -0800

    Remove unused matlab and scripts folder in root directory

--- matlab/Readme.md
+++
@@ -1,3 +0,0 @@
-### Matlab Scripts
-
-These Matlab scripts are used to test and verify solutions of the solver algorithms.
--- script/listComponents.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-Dir.chdir('../src/component/components/') do
-  Dir.glob('*.coffee').each do |item|
-    fname = item.gsub('.coffee', '')
-    puts "#{fname}: 'cs!src/component/#{fname}',"
-  end
-end
